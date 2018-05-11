467446263b8933818435758df91af283ade56f30
Michael Petö <michael@petoe.me>
2018-05-11 17:35:01 +0200

    Continue rework of shard - add SafeBox(T)#to_* methods

--- src/overflow.cr
+++ src/overflow.cr
@@ -3,7 +3,7 @@ require "./overflow/intrinsics"
 require "./overflow/exception"
 require "./overflow/safebox"
 require "./overflow/operator"
-# require "./overflow/conversion"
+require "./overflow/conversion"
 
 s = SafeBox.new 5_i8
 pp s
@@ -16,4 +16,8 @@ pp (s + b)
 c = SafeBox.new Int8::MAX
 pp c
 
-pp (s + c)
+# pp (s + c)
+
+f = SafeBox.new 300_u32
+pp f
+pp f.to_i8
--- src/overflow/conversion.cr
+++ src/overflow/conversion.cr
@@ -1,54 +1,15 @@
-{% begin %}
-  {% sizes = [8, 16, 32, 64, 128] %}
-  {% ints = {to_i: 32, to_i8: 8, to_i16: 16, to_i32: 32, to_i64: 64, to_i128: 128} %}
-  {% uints = {to_u: 32, to_u8: 8, to_u16: 16, to_u32: 32, to_u64: 64, to_u128: 128}%}
-  {% for size in sizes %}
-    struct Int{{size.id}}
-      {% for method, method_size in ints %}
-        def {{method.id}} : Int{{method_size.id}}
-          {% if method_size >= size %}
-            previous_def
-          {% else %}
-            return previous_def if Int{{method_size.id}}::MIN <= self <= Int{{method_size.id}}::MAX
-            raise IntegerOverflow.new "#{self} too big or small for Int{{size.id}}\u0023{{method.id}}"
-          {% end %}
-        end
-      {% end %}
-    
-      {% for method, method_size in uints %}
-        def {{method.id}} : UInt{{method_size.id}}
-          {% if method_size >= size %}
-            return previous_def if UInt{{method_size.id}}::MIN <= self
-          {% else %}
-            return previous_def if UInt{{method_size.id}}::MIN <= self <= UInt{{method_size.id}}::MAX
-          {% end %}
-          raise IntegerOverflow.new "#{self} too big or small for Int{{size.id}}\u0023{{method.id}}"
-        end
-      {% end %}
-    end
-
-    struct UInt{{size.id}}
-      {% for method, method_size in uints %}
-        def {{method.id}} : UInt{{method_size.id}}
-          {% if method_size >= size %}
-            previous_def
-          {% else %}
-            return previous_def if self <= UInt{{method_size.id}}::MAX
-            raise IntegerOverflow.new "#{self} too big for UInt{{size.id}}\u0023{{method.id}}"
-          {% end %}
-        end
-      {% end %}
-
-      {% for method, method_size in ints %}
-        def {{method.id}} : Int{{method_size.id}}
-          {% if method_size > size %}
-            previous_def
-          {% else %}
-            return previous_def if self <= Int{{method_size.id}}::MAX
-            raise IntegerOverflow.new "#{self} too big for UInt{{size.id}}\u0023{{method.id}}"
-          {% end %}
-        end
-      {% end %}
-    end
+struct SafeBox(T)
+  {% begin %}
+    {% ints = {to_i: Int32, to_i8: Int8, to_i16: Int16, to_i32: Int32, to_i64: Int64, to_i128: Int128, to_u: UInt32, to_u8: UInt8, to_u16: UInt16, to_u32: UInt32, to_u64: UInt64, to_u128: UInt128} %}
+    {% for method, type in ints %}
+      def {{method.id}} : {{type}}
+        \{% if Int::Unsigned.union_types.includes?(@type.type_vars.first) %}
+          return @value.{{method.id}} if @value <= {{type}}::MAX
+        \{% else %}
+          return @value.{{method.id}} if {{type}}::MIN <= @value <= {{type}}::MAX
+        \{% end %}
+        raise IntegerOverflow.new "#{typeof(self)}\u0023{{method.id}}"
+      end
+    {% end %}
   {% end %}
-{% end %}
+end
