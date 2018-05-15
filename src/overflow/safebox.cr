abb60b6813ae5ba670b262a6a37a35b3088175de
Michael Petö <michael@petoe.me>
2018-05-15 15:02:29 +0200

    Add type instance var

--- src/overflow/safebox.cr
+++ src/overflow/safebox.cr
@@ -1,7 +1,10 @@
 struct SafeBox(T)
   getter value : T
+  getter type : T.class
 
   forward_missing_to @value
 
-  def initialize(@value : T); end
+  def initialize(@value : T)
+    @type = T
+  end
 end
